c6a2f6b5a69928641ecfce3a5269c74ec9b940da
Mike Cowell <spike@spikeandbetty.com>
2022-11-04 10:43:24 -0400

    init commit

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isData;
+            Console.WriteLine("Welcome. Please select your database. It's much less fun than selecting a mage or fighter, but who am I to complain?");
+            Console.WriteLine("-------------------------------");
+            do
+            {
+                Console.WriteLine("Please choose between SQL, List or Mongo.");
+                var ans = Console.ReadLine().ToLower();
+                if(ans == "sql" || ans == "list" || ans == "mongo")
+                {
+                    isData = true;
+                    var data = DataAccessFactory.GetDataAccessType(ans);
+                    data.SaveData();
+                    data.LoadData();
+                }
+                else
+                {
+                    Console.WriteLine("Come on, buddy. I don't want to be here any more than you do. Stop messing about.");
+                    isData = false;
+                }
+            }
+            while (isData== false);
         }
     }
 }
